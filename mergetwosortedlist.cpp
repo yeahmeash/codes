27ac15f7dfc9dc3d37fb66bb95478f3472e4882e
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2023-03-18 17:45:35 +0530

    Create mergetwosortedlist.cpp

---
+++ mergetwosortedlist.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        if (l1) {
+            tail->next = l1;
+        } else {
+            tail->next = l2;
+        }
+
+        ListNode* head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
