7102835cf02b786ef6ab093cfcfb754535b55e89
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2023-07-16 00:36:57 +0530

    Update MaximumandMinimumofanArray.cpp

--- MaximumandMinimumofanArray.cpp
+++ MaximumandMinimumofanArray.cpp
@@ -13,68 +13,69 @@ Output:  Minimum element is: 3
 *******************************************************************************/
 
 // C++ program of above implementation
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// Pair struct is used to return
-// two values from getMinMax()
-struct Pair
+void getMinMax(int arr[], int n, int& minVal, int& maxVal)
 {
-	int min;
-	int max;
-};
-
-Pair getMinMax(int arr[], int n)
-{
-	struct Pair minmax;	
-	int i;
-	
-	// If there is only one element
-	// then return it as min and max both
-	if (n == 1)
-	{
-		minmax.max = arr[0];
-		minmax.min = arr[0];	
-		return minmax;
-	}
-	
-	// If there are more than one elements,
-	// then initialize min and max
-	if (arr[0] > arr[1])
-	{
-		minmax.max = arr[0];
-		minmax.min = arr[1];
-	}
-	else
-	{
-		minmax.max = arr[1];
-		minmax.min = arr[0];
-	}
-	
-	for(i = 2; i < n; i++)
-	{
-		if (arr[i] > minmax.max)	
-			minmax.max = arr[i];
-			
-		else if (arr[i] < minmax.min)	
-			minmax.min = arr[i];
-	}
-	return minmax;
+    int i;
+    
+    if (n % 2 == 0)
+    {
+        if (arr[0] > arr[1])
+        {
+            maxVal = arr[0];
+            minVal = arr[1];
+        }
+        else
+        {
+            minVal = arr[0];
+            maxVal = arr[1];
+        }
+        
+        i = 2;
+    }
+    else
+    {
+        minVal = arr[0];
+        maxVal = arr[0];
+        
+        i = 1;
+    }
+    
+    while (i < n - 1)
+    {       
+        if (arr[i] > arr[i + 1])
+        {
+            if (arr[i] > maxVal)
+                maxVal = arr[i];
+                
+            if (arr[i + 1] < minVal)
+                minVal = arr[i + 1];
+        }
+        else
+        {
+            if (arr[i + 1] > maxVal)
+                maxVal = arr[i + 1];
+                
+            if (arr[i] < minVal)
+                minVal = arr[i];
+        }
+        
+        i += 2;
+    }   
 }
 
-// Driver code
 int main()
 {
-	int arr[] = { 1000, 11, 445,
-				1, 330, 3000 };
-	int arr_size = 6;
-	
-	struct Pair minmax = getMinMax(arr, arr_size);
-	
-	cout << "Minimum element is "
-		<< minmax.min << endl;
-	cout << "Maximum element is "
-		<< minmax.max;
-		
-	return 0;
+    int arr[] = { 1000, 11, 445, 1, 330, 3000 };
+    int arr_size = 6;
+    
+    int minVal, maxVal;
+    getMinMax(arr, arr_size, minVal, maxVal);
+    
+    cout << "Minimum element is " << minVal << endl;
+    cout << "Maximum element is " << maxVal;
+    
+    return 0;
 }
