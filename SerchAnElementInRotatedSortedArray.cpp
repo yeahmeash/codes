b1e183b10d1c40cea1f5d8e742912dad0cda9a4d
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2023-08-07 12:45:27 +0530

    Update SerchAnElementInRotatedSortedArray.cpp

--- SerchAnElementInRotatedSortedArray.cpp
+++ SerchAnElementInRotatedSortedArray.cpp
@@ -49,4 +49,34 @@ int main()
 		cout << "Key not found";
 }
 
-// This code is contributed by Aditya Kumar (adityakumar129)
+
+class Solution {
+public:
+int search(vector<int>&a,int target){
+ int low=0;
+ int  high=a.size()-1;
+ while ( low <= high ) {
+     int mid=(low+high)/2;
+     if(a[mid]==target){
+         return mid;
+     }
+     if(a[low]<=a[mid]){
+         if(target>=a[low]&&target<=a[mid]){
+             high=mid-1;
+         }
+             else{
+                 low=mid+1;
+             }  
+         }
+         else{
+ if(target>=a[mid] && target<=a[high]){
+              low=mid+1;
+         }
+             else{
+               high=mid-1; 
+              } 
+     }
+ }
+ return -1;
+}
+    };
