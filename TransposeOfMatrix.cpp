61cec0fa5ad3b4f577f248e372043ddf464ff717
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2023-08-09 11:18:45 +0530

    Create TransposeOfMatrix.cpp

---
+++ TransposeOfMatrix.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{   
+    public:  
+    //Function to find transpose of a matrix.
+    void transpose(vector<vector<int>>& matrix, int n) {
+        vector<vector<int>> temp(n, vector<int>(n)); // Allocate memory for transposed matrix
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                temp[i][j] = matrix[j][i];
+            }
+        }
+
+        matrix = temp; // Update mat to point to the transposed matrix
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n,vector<int>(n)); 
+
+        for(int i=0; i<n; i++)
+        {
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        ob.transpose(matrix,n);
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+                cout<<matrix[i][j]<<" ";
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
