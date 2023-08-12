09af984e04918a58a278aa273b1848025cb7a636
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2023-08-12 12:50:26 +0530

    Update isPalindrome.cpp

--- isPalindrome.cpp
+++ isPalindrome.cpp
@@ -77,5 +77,99 @@ int main() {
     return 0;
 }
 
+Print all possible permutations of series, both iteratively and Recursively.
+
+    1. Iterative Approach:
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+The program enters a do-while loop, where it:
+
+Prints the contents of the nums vector: 1 2 3
+Moves to the next permutation using next_permutation(nums.begin(), nums.end()). This will change the nums vector to the next lexicographically greater 
+permutation, or it will return false if no further permutation is possible.
+This loop continues until all permutations are exhausted.
+    All possible permutations (iterative):
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+
+
+void printPermutationsIterative(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+
+    do {
+        for (int num : nums) {
+            cout << num << " ";
+        }
+        cout << endl;
+    } while (next_permutation(nums.begin(), nums.end()));
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    cout << "All possible permutations (iterative):" << endl;
+    printPermutationsIterative(nums);
+
+    return 0;
+}
+recursive solution
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void swap(vector<int>& nums, int i, int j) {
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+void generatePermutationsRecursive(vector<int>& nums, int start) {
+    if (start == nums.size() - 1) {
+        for (int num : nums) {
+            cout << num << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    for (int i = start; i < nums.size(); ++i) {
+        swap(nums, start, i);
+        generatePermutationsRecursive(nums, start + 1);
+        swap(nums, start, i); // Backtrack
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    cout << "All possible permutations (recursive):" << endl;
+    generatePermutationsRecursive(nums, 0);
+
+    return 0;
+}
 
 
