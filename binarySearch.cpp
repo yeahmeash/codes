ad8f38ce115669afe8d0fbf393384e4047ef5270
Aishwarya sharma <116885835+yeahmeash@users.noreply.github.com>
2024-09-04 15:08:11 +0530

    Update binarySearch.java

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,32 +1,77 @@
 1. Iteration Method
 
-    binarySearch(arr, x, low, high)
-        repeat till low = high
-               mid = (low + high)/2
-                   if (x == arr[mid])
-                   return mid
-   
-                   else if (x > arr[mid]) // x is on the right side
-                       low = mid + 1
-   
-                   else                  // x is on the left side
-                       high = mid - 1
+   public class BinarySearch {
+    public static int binarySearch(int[] arr, int x, int low, int high) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if x is present at mid
+            if (arr[mid] == x)
+                return mid;
+
+            // If x greater, ignore left half
+            if (arr[mid] < x)
+                low = mid + 1;
+
+            // If x is smaller, ignore right half
+            else
+                high = mid - 1;
+        }
+
+        // If we reach here, then the element was not present
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 10, 40};
+        int x = 10;
+        int result = binarySearch(arr, x, 0, arr.length - 1);
+        if (result == -1)
+            System.out.println("Element not present in array");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
+
 2. Recursive Method (The recursive method follows the divide and conquer approach)
 
-    binarySearch(arr, x, low, high)
-           if low > high
-               return False 
-   
-           else
-               mid = (low + high) / 2 
-                   if x == arr[mid]
-                   return mid
-       
-               else if x > arr[mid]        // x is on the right side
-                   return binarySearch(arr, x, mid + 1, high)
-               
-               else                        // x is on the left side
-                   return binarySearch(arr, x, low, mid - 1) 
+   public class BinarySearch {
+    public static int binarySearch(int[] arr, int x, int low, int high) {
+        if (low > high) {
+            return -1; // Element is not present in the array
+        }
+
+        int mid = low + (high - low) / 2;
+
+        // Check if x is present at mid
+        if (arr[mid] == x) {
+            return mid;
+        }
+
+        // If x is greater, ignore the left half
+        if (arr[mid] < x) {
+            return binarySearch(arr, x, mid + 1, high);
+        }
+
+        // If x is smaller, ignore the right half
+        return binarySearch(arr, x, low, mid - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 10, 40};
+        int x = 10;
+        int result = binarySearch(arr, x, 0, arr.length - 1);
+        if (result == -1) {
+            System.out.println("Element not present in array");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
   Recursive implementation of Binary Search:
 // C++ program to implement recursive Binary Search
 #include <bits/stdc++.h>
@@ -71,50 +116,7 @@ int main(void)
 		: cout << "Element is present at index " << result;
 	return 0;
 }
-java recursive binary approach 
-// Java implementation of recursive Binary Search
-class BinarySearch {
-	// Returns index of x if it is present in arr[l..
-	// r], else return -1
-	int binarySearch(int arr[], int l, int r, int x)
-	{
-		if (r >= l) {
-			int mid = l + (r - l) / 2;
 
-			// If the element is present at the
-			// middle itself
-			if (arr[mid] == x)
-				return mid;
-
-			// If element is smaller than mid, then
-			// it can only be present in left subarray
-			if (arr[mid] > x)
-				return binarySearch(arr, l, mid - 1, x);
-
-			// Else the element can only be present
-			// in right subarray
-			return binarySearch(arr, mid + 1, r, x);
-		}
-
-		// We reach here when element is not present
-		// in array
-		return -1;
-	}
-
-	// Driver method to test above
-	public static void main(String args[])
-	{
-		BinarySearch ob = new BinarySearch();
-		int arr[] = { 2, 3, 4, 10, 40 };
-		int n = arr.length;
-		int x = 10;
-		int result = ob.binarySearch(arr, 0, n - 1, x);
-		if (result == -1)
-			System.out.println("Element not present");
-		else
-			System.out.println("Element found at index "
-							+ result);
-	}
   
   Iterative implementation of Binary Search 
   // C++ program to implement iterative Binary Search
@@ -158,49 +160,5 @@ int main(void)
 		: cout << "Element is present at index " << result;
 	return 0;
 }
-// Java implementation of iterative Binary Search
-class BinarySearch {
-	// Returns index of x if it is present in arr[],
-	// else return -1
-	int binarySearch(int arr[], int x)
-	{
-		int l = 0, r = arr.length - 1;
-		while (l <= r) {
-			int m = l + (r - l) / 2;
 
-			// Check if x is present at mid
-			if (arr[m] == x)
-				return m;
-
-			// If x greater, ignore left half
-			if (arr[m] < x)
-				l = m + 1;
-
-			// If x is smaller, ignore right half
-			else
-				r = m - 1;
-		}
-
-		// if we reach here, then element was
-		// not present
-		return -1;
-	}
-
-	// Driver method to test above
-	public static void main(String args[])
-	{
-		BinarySearch ob = new BinarySearch();
-		int arr[] = { 2, 3, 4, 10, 40 };
-		int n = arr.length;
-		int x = 10;
-		int result = ob.binarySearch(arr, x);
-		if (result == -1)
-			System.out.println("Element not present");
-		else
-			System.out.println("Element found at "
-							+ "index " + result);
-	}
-}
-
-}
 
